983204238d1e6ebcf2b82ad8f47d58aee3acc92b
Timo Tijhof <krinklemail@gmail.com>
2012-10-29 23:31:00 +0100

    sync dotfiles

--- hosts/KrinkleMac/modules/setup.bash
+++ hosts/KrinkleMac/modules/setup.bash
@@ -70,3 +70,8 @@ export PATH=/usr/local/bin:$PATH
 # $ sudo mv /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin /Library/Internet\ Plug-Ins/disabled
 # $ sudo ln -sf /System/Library/Java/Support/Deploy.bundle/Contents/Resources/JavaPlugin2_NPAPI.plugin /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin
 # $ sudo ln -sf /System/Library/Frameworks/JavaVM.framework/Commands/javaws /usr/bin/javaws
+
+## Ubuntu sometimes thinks the Mac Terminal doesn't support colors
+## (through TERM and /usr/bin/tput)
+## Default TERM in Apple's Terminal.app: xterm-256color
+## Changed to (in Preferences): rxvt
