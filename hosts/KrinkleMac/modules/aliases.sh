08fdbede5a5e7e8874166ab7955490fa33623cce
Timo Tijhof <krinklemail@gmail.com>
2013-05-02 22:45:14 +0200

    sync dotfiles

--- hosts/KrinkleMac/modules/aliases.sh
+++ hosts/KrinkleMac/modules/aliases.sh
@@ -1,13 +1,9 @@
-# Actions
+# Version control
 alias docheckcommitmsg='cat ~/Development/tmp/COMMIT.txt'
 alias dosvncommit='svn commit -F ~/Development/tmp/COMMIT.txt'
 alias dogitcommit='git commit -F ~/Development/tmp/COMMIT.txt'
 alias gogogerrit='git review -R'
 alias grabfromgerrit='git review -d'
-alias dotfiles-push='cd ~/.krinkle.dotfiles && git commit -a -m "sync dotfiles" && git push -f && cd -'
-alias dotfiles-push-amend='cd ~/.krinkle.dotfiles && git commit -a --amend -c HEAD --reset-author && git push -f && cd -'
-
-# Set up stuff for git repositories from gerrit.wikimedia.org
 alias dogerritfixup='git review -s && git remote rm origin 2> /dev/null; git remote update && git branch --set-upstream-to gerrit/master master'
 
 # http://ariejan.net/2011/11/08/fixing-a-slow-starting-terminal-or-iterm2-on-mac-os-x
@@ -18,7 +14,8 @@ alias dotruncateasl='sudo rm /var/log/asl/*.asl'
 # https://snipt.net/Yannick/delete-recursively-a-file-ds_store-thumbsdb-desktopini-etc/
 alias dormdsstore='find . -name ".DS_Store" -type f -exec rm {} \;'
 
-alias doapachereset='cd /usr/local/opt/httpd/var/apache2/log && rm -f *_log && sudo apachectl restart && l'
+# Web server
+alias doapachereset='cd /var/log/httpd && rm -f *_log && sudo apachectl restart && l'
 
 # Mirror media repositories
 # $ wget-magic-noindex "http://..."
