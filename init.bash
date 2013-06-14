927ceea35a5b134c9247446d6f4ecde549d390c5
Timo Tijhof <krinklemail@gmail.com>
2013-06-14 12:16:59 -0700

    sync dotfiles from jquery conf

--- init.bash
+++ init.bash
@@ -62,7 +62,7 @@ then
 	echo "${CLR_LINE}${CLR_BOLD}Provisioning $KDF_CANONICAL_HOST$CLR_NONE"
 	echo
 	echo "$CLR_MAGENTA>>$CLR_NONE Recognised host as $KDF_CANONICAL_HOST"
-	ret=$(_dotfiles-prompt-choice "$CLR_MAGENTA>>$CLR_NONE Run host specific privison?")
+	ret=$(_dotfiles-prompt-choice "$CLR_MAGENTA>>$CLR_NONE Run host specific provision?")
 	if [[ -n $ret ]]
 	then
 		$KDF_HOST_INIT
